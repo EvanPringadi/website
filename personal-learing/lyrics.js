d6782fc40900845c0c64ecc7cb7ba9996da20c3a
Cool <evan.ashley11@gmail.com>
2024-08-09 15:10:52 +0700

    modif lyrics

--- personal-learing/lyrics.js
+++ personal-learing/lyrics.js
@@ -1,5 +1,8 @@
 const lyricsElement = document.getElementById('lyrics');
-const lyrics = 'Twinkle twinkle little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nWhen the blazing sun is gone,\nWhen he nothing shines upon,\nThen you show your little light,\nTwinkle twinkle all the night.';
+const lyrics = `I don't care, go on and tear me apart
+I don't care if you do, ooh-ooh, ooh
+'Cause in a sky, 'cause in a sky full of stars
+I think I saw you`;
 
 let typedText = '';
 
@@ -7,7 +10,7 @@ const typeWriter = () => {
   if (typedText.length < lyrics.length) {
     typedText += lyrics[typedText.length];
     lyricsElement.textContent = typedText;
-    setTimeout(typeWriter, 10); // Adjust speed here (lower value = faster typing)
+    setTimeout(typeWriter, 70); // Adjust speed here (lower value = faster typing)
   } else {
     // Restart animation when all lyrics are typed
     typedText = '';
