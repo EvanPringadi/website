ec1871d6b44499671d8ccd23204cf6abc20a6eb4
Cool <evan.ashley11@gmail.com>
2024-08-17 20:58:44 +0700

    Learn promises and convert code to promises

--- course 22h/javascript-amazon-project-main/data/cart.js
+++ course 22h/javascript-amazon-project-main/data/cart.js
@@ -84,3 +84,15 @@ export function updateQuantity(productId, newQuantity) {
     matchingItem.quantity = newQuantity;
   });
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
