339f29e5deb77c5d1c44515ea0fc0781a9f29c89
Cool <evan.ashley11@gmail.com>
2024-09-27 13:37:40 +0700

    program menghitung volume balok

--- Java/Test.java
+++ Java/Test.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Test { 
   public static void main(String args[]) 
   { 
@@ -6,5 +8,22 @@ public class Test {
 
       String name = "Evan";
       System.out.println(name);
+
+      System.out.println("Silakan isi panjang balok...");
+      Scanner scannerPanjang = new Scanner(System.in);
+      float panjang = scannerPanjang.nextFloat();
+
+      System.out.println("Silakan isi lebar balok...");
+      Scanner scannerLebar = new Scanner(System.in);
+      float lebar = scannerLebar.nextFloat();
+
+      System.out.println("Silakan isi tinggi balok...");
+      Scanner scannerTinggi = new Scanner(System.in);
+      float tinggi = scannerTinggi.nextFloat();
+
+      float volume = panjang * lebar * tinggi; 
+
+      System.out.println("Volume balok tersebut adalah " + volume);
+
   } 
 }
