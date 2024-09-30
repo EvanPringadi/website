6a1949f20766cc89ab92f031b444c7f8b9cd209f
Cool <evan.ashley11@gmail.com>
2024-09-30 09:48:48 +0700

    moving some files and try to make sorting system in javascript

--- Java/Test.java
+++ Java/Test.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Test { 
-  public static void main(String args[]) 
-  { 
+  public static void main(String args[]) { 
       System.out.println("Hello World"); 
       System.out.println("What is your name?");
 
---
+++ JavaScript/sorting.js
@@ -0,0 +1,38 @@
+console.log("hello");
+let num = [2, 2, 5, 1, 4];
+let number = [55, 12, 4, 17, 77, 12];
+
+console.log(sort(num));
+console.log(sort(number));
+
+function getLowestNumber(num) {
+  let lowest_num = num[0];
+  for (let i=0; i < num.length; i++) {
+    curr_num = num[i];
+    if (lowest_num > curr_num) {
+      lowest_num = curr_num;
+    }
+  }
+  return lowest_num;
+}
+
+function sort(num) {
+  let sorted_num = [];
+  while (num.length > 0) {
+    let lowest = getLowestNumber(num);
+    sorted_num.push(lowest);
+    // Remove all instances of the lowest number
+
+    // Find the index of the first occurrence of the lowest number and remove it
+    //let index = num.indexOf(lowest);
+    let index;
+    for (let i=0; i<num.length; i++) {
+      if (num[i] == lowest){
+        index = i;
+      }
+    }
+
+    num.splice(index, 1);
+  }
+  return sorted_num;
+}
