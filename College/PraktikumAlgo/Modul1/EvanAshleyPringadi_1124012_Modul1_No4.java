ad5bd374945b15978a0c86ecc151214ad12618de
Cool <evan.ashley11@gmail.com>
2024-10-03 18:17:18 +0700

    refinement tugas modul 1 no 4

--- College/PraktikumAlgo/Modul1/EvanAshleyPringadi_1124012_Modul1_No4.java
+++ College/PraktikumAlgo/Modul1/EvanAshleyPringadi_1124012_Modul1_No4.java
@@ -5,22 +5,11 @@ public class EvanAshleyPringadi_1124012_Modul1_No4 {
     Scanner sc = new Scanner(System.in);
     System.out.println("Silakan Isi Waktu dalam Detik...");
     int totalDetik = sc.nextInt();
-    int sisaDetik = totalDetik;
-    int jumlahJam = 0;
-    int jumlahMenit = 0;
-    int jumlahDetik = 0;
+    int jumlahJam = totalDetik / 3600;
+    int sisaDetik = totalDetik % 3600;
+    int jumlahMenit = sisaDetik/60;
+    int jumlahDetik = sisaDetik % 60;
 
-    if (totalDetik >= 3600){
-      jumlahJam = totalDetik / 3600;
-      sisaDetik = totalDetik % 3600;
-    }
-    if (sisaDetik >= 60) {
-      jumlahMenit = sisaDetik/60;
-      sisaDetik = sisaDetik % 60;
-    }
-    if (sisaDetik >= 0) {
-      jumlahDetik = sisaDetik;
-    }
     System.out.println("Jam = " + jumlahJam + ", Menit = " + jumlahMenit + ", Detik = " + jumlahDetik);
     System.out.println(totalDetik + " detik adalah " + jumlahJam + " jam, " + jumlahMenit + " menit, dan " + jumlahDetik + " detik.");
   }
